7b1c212b0efc8882e1a8622e9265b90965b43cab
islamhanafi94 <islamhanafi1994@gmail.com>
2020-03-25 20:48:32 +0200

    lab1

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,24 @@
+class PostsController < ApplicationController
+
+    def index
+    end
+  
+    def new
+    end
+  
+    def creat
+    end
+
+    def show
+    end
+  
+    
+    def edit
+    end
+    
+    def update
+    end
+    
+    def destroy
+    end
+end
